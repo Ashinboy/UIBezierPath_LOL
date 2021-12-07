e039f43c154e9c47d93199de978fe5bdcd6d19ec
Ashin <ashinwangboy@gmail.com>
2021-12-07 18:58:45 +0800

    change text

---
+++ UIBezierPath_LOL/SharpImageView.swift
@@ -0,0 +1,36 @@
+//
+//  SharpImageView.swift
+//  UIBezierPath_LOL
+//
+//  Created by Ashin Wang on 2021/12/7.
+//
+
+import UIKit
+
+class SharpImageView: UIImageView {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let path = UIBezierPath()
+        path.move(to: CGPoint.zero)
+        path.addLine(to: CGPoint(x: bounds.width, y: 0))
+        path.addLine(to: CGPoint(x: bounds.width, y: bounds.height))
+        //path.addLine(to: CGPoint(x: 1000 , y: bounds.width))
+        path.addLine(to: CGPoint(x: 0, y: bounds.height * 0.8))
+        path.close()
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        layer.mask = shapeLayer
+        
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
